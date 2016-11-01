56cb504ff0560dff523b8e7224f6773d09aadf2b
Stefan Andersson <stefan@tocaboca.com>
2016-11-01 22:39:22 +0100

    Did some work on a grid-shader.

---
+++ Assets/Scripts/GridSelectShaderController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridSelectShaderController : MonoBehaviour {
+
+    public static GridSelectShaderController Instance;
+    private MeshRenderer rend;
+    private Vector4 min;
+    private Vector4 max;
+    private Material selectMat;
+    private int propertyID1;
+    private int propertyID2;
+
+
+    void Awake() {
+        Instance = this;
+        rend = GetComponent<MeshRenderer>();
+        selectMat = rend.material;
+
+        propertyID1 = Shader.PropertyToID("_Pos1");
+        propertyID2 = Shader.PropertyToID("_Pos2");
+        min = new Vector4();
+        max = new Vector4();
+    }
+
+    void Start() {
+        rend.material.mainTexture = new Texture2D((int)Grid.Instance.GridWorldSize.x * Grid.TILE_RESOLUTION, (int)Grid.Instance.GridWorldSize.y * Grid.TILE_RESOLUTION, TextureFormat.RGBA32, false);
+        rend.material.mainTexture.wrapMode = TextureWrapMode.Clamp;
+        rend.material.mainTexture.filterMode = FilterMode.Point;
+        rend.transform.localScale = new Vector3((int)Grid.Instance.GridWorldSize.x, (int)Grid.Instance.GridWorldSize.y, 1);
+    }
+
+    public void SetSelectSize(Tile _tile1, Tile _tile2) {
+        min.x = Mathf.Min(_tile1.GridX, _tile2.GridX);
+        min.y = Mathf.Min(_tile1.GridY, _tile2.GridY);
+        max.x = Mathf.Max(_tile1.GridX, _tile2.GridX);
+        max.y = Mathf.Max(_tile1.GridY, _tile2.GridY);
+
+        selectMat.SetVector(propertyID1, min);
+        selectMat.SetVector(propertyID2, max);
+    }
+}
---
+++ Assets/Shaders/GridSelect.shader
@@ -0,0 +1,64 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Custom/GridSelect"
+{
+	Properties
+	{
+		_Color ("Color", Color) = (1, 0, 0, 0.5)
+		_Pos1 ("Pos #1", Vector) = (0, 0, 0)
+		_Pos2 ("Pos #2", Vector) = (0, 0, 0)
+	}
+	SubShader
+	{
+		Tags { "Queue"="Transparent" }
+
+		Pass
+		{
+			ZWrite Off
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float4 worldPos : TEXCOORD0;
+			};
+
+			float4 _Color;
+			uniform float3 _Pos1;
+			uniform float3 _Pos2;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				if ((i.worldPos.x > _Pos1.x && i.worldPos.x < _Pos2.x && i.worldPos.y > _Pos1.y && i.worldPos.y < _Pos2.y)
+					|| (i.worldPos.x < _Pos1.x && i.worldPos.x > _Pos2.x && i.worldPos.y < _Pos2.y && i.worldPos.y > _Pos2.y))
+				{
+					return _Color;
+				}
+				else 
+				{
+					return float4(0, 0, 0, 0);
+				}
+			}
+			ENDCG
+		}
+	}
+}
