a3a411376b532587b2a7db6b7046e31581f27487
Stefan Andersson <stefan@tocaboca.com>
2017-03-11 17:11:47 +0100

    Made UVController update mesh in editor.

---
+++ Assets/Editor/UVControllerEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(UVController))]
+public class UVControllerEditor : Editor {
+	UVController uvc;
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI ();
+
+		uvc = (UVController)target;
+		if (GUI.changed) {
+			uvc.Setup ();
+			uvc.ChangeAsset (uvc.Coordinates);
+		}
+	}
+}
