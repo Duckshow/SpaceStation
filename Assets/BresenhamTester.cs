2500045a1d2007625a3ff86b27473ac76ed3094b
Stefan Andersson <stefan@tocaboca.com>
2017-05-29 09:34:32 +0200

    Cleaned up BresenhamsTester.

--- Assets/BresenhamTester.cs
+++ Assets/BresenhamTester.cs
@@ -10,20 +10,9 @@ public class BresenhamTester : MonoBehaviour {
 	public int Resolution;
 	public int Resolution2;
 
-	public GameObject Colliders;
-
 	private float resolution;
 	private Vector2 resPos;
 
-
-	void Start(){
-		int y = 0;
-		for(int i = 0; i < Colliders.transform.childCount; i++){
-			Colliders.transform.GetChild(i).transform.localPosition = new Vector3(i % 30, y, 0);
-			if(i % 30 == 0)
-				y++;
-		}
-	}
 
 	[EasyButtons.Button]
 	public void DrawLine(){
@@ -33,7 +22,7 @@ public class BresenhamTester : MonoBehaviour {
 		resolution = 1 / (float)Resolution;
 		BresenhamsLine bres = new BresenhamsLine(StartPos, (StartPos + (Angle * Length)), Resolution);
 		foreach(Vector2 _p in bres){
-			resPos = new Vector2(_p.x * resolution, _p.y * resolution);
+			resPos = new Vector2(_p.x, _p.y);
 			Debug.DrawLine(new Vector2(resPos.x - (resolution * 0.5f), resPos.y - (resolution * 0.5f)), new Vector2(resPos.x + (resolution * 0.5f), resPos.y - (resolution * 0.5f)), Color.cyan);
 			Debug.DrawLine(new Vector2(resPos.x + (resolution * 0.5f), resPos.y - (resolution * 0.5f)), new Vector2(resPos.x + (resolution * 0.5f), resPos.y + (resolution * 0.5f)), Color.cyan);
 			Debug.DrawLine(new Vector2(resPos.x + (resolution * 0.5f), resPos.y + (resolution * 0.5f)), new Vector2(resPos.x - (resolution * 0.5f), resPos.y + (resolution * 0.5f)), Color.cyan);
@@ -44,7 +33,7 @@ public class BresenhamTester : MonoBehaviour {
 		bres = new BresenhamsLine(StartPos, (StartPos + (Angle * Length)), Resolution2);
 		foreach(Vector2 _p in bres){
 			Debug.Log(resPos);
-			resPos = new Vector2(_p.x * resolution, _p.y * resolution);
+			resPos = new Vector2(_p.x, _p.y);
 			Debug.DrawLine(new Vector2(resPos.x - (resolution * 0.5f), resPos.y - (resolution * 0.5f)), new Vector2(resPos.x + (resolution * 0.5f), resPos.y - (resolution * 0.5f)), Color.green);
 			Debug.DrawLine(new Vector2(resPos.x + (resolution * 0.5f), resPos.y - (resolution * 0.5f)), new Vector2(resPos.x + (resolution * 0.5f), resPos.y + (resolution * 0.5f)), Color.green);
 			Debug.DrawLine(new Vector2(resPos.x + (resolution * 0.5f), resPos.y + (resolution * 0.5f)), new Vector2(resPos.x - (resolution * 0.5f), resPos.y + (resolution * 0.5f)), Color.green);
@@ -52,20 +41,20 @@ public class BresenhamTester : MonoBehaviour {
 		}
 	}
 
-	List<Collider2D> results = new List<Collider2D>();
-	public LayerMask mask;
-	[EasyButtons.Button]
-	public void ColliderTest(){
-		results.Clear();
+	// List<Collider2D> results = new List<Collider2D>();
+	// public LayerMask mask;
+	// [EasyButtons.Button]
+	// public void ColliderTest(){
+	// 	results.Clear();
 
-		for(int i = 0; i < Colliders.transform.childCount; i++){
-			if((Colliders.transform.GetChild(i).position - transform.position).magnitude < Length)
-				results.Add(Colliders.transform.GetChild(i).GetComponent<Collider2D>());
-		}
+	// 	for(int i = 0; i < Colliders.transform.childCount; i++){
+	// 		if((Colliders.transform.GetChild(i).position - transform.position).magnitude < Length)
+	// 			results.Add(Colliders.transform.GetChild(i).GetComponent<Collider2D>());
+	// 	}
 
 		
-		for(int i = 0; i < results.Count; i++){
-			results[i].gameObject.SetActive(true);
-		}
-	}
+	// 	for(int i = 0; i < results.Count; i++){
+	// 		results[i].gameObject.SetActive(true);
+	// 	}
+	// }
 }
